9c22afc5e722dbd41a36caec98c761efe7f45308
Muneeb <muneebkhalid98@gmail.com>
2025-05-06 16:38:06 +0500

    Added ORM

--- ecom_app/urls.py
+++ ecom_app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
+from .views import ProductByUserView
 from rest_framework.routers import DefaultRouter
 from ecom_app.views import (
     UserViewSet,
     ProductViewSet,
     OrderViewSet,
-    OrderItemViewSet
+    OrderItemViewSet,
+    OrderWithTotalQuantityView,
+    PopularProduct,
+    TotalQuantitySold,
+    TotalRevenuePerProductView,
 )
 
 router = DefaultRouter()
@@ -15,4 +20,9 @@ router.register(r'order-items', OrderItemViewSet, basename='orderitem')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('products/user/<int:user_id>/', ProductByUserView.as_view(), name='product-by-user'),
+    path('orders/user/<int:user_id>/total-quantity/', OrderWithTotalQuantityView.as_view(), name='order-with-total-quantity'),
+    path('products/mostpop',PopularProduct.as_view(),name='most-popular'),
+    path('products/mostsold',TotalQuantitySold.as_view(),name='totalquantitysold'),
+     path('products/mostrevenue/', TotalRevenuePerProductView.as_view(), name='most-revenue-products'),
 ]
--- ecom_app/views.py
+++ ecom_app/views.py
@@ -4,6 +4,11 @@ from ecom_app.serializers import UserSerializer, ProductSerializer, OrderItemSer
 from rest_framework import generics, permissions, viewsets, renderers
 from ecom_app.models import Product, Order, OrderItem
 from rest_framework.exceptions import ValidationError
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Sum, F
+
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -45,3 +50,90 @@ class OrderItemViewSet(viewsets.ModelViewSet):
 
         product.stock -= order_item.quantity
         product.save()
+
+
+    
+class ProductByUserView(APIView):
+    def get(self, request, user_id):
+        products = Product.objects.filter(owner__id=user_id)
+        
+        if not products:
+            return Response({"detail": "No products found for this user."}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+
+class OrderWithTotalQuantityView(APIView):
+    def get(self, request, user_id):
+    
+        orders = Order.objects.filter(user__id=user_id).annotate(total_quantity=Sum('items__quantity'))
+        
+        if not orders:
+            return Response({"detail": "No orders found for this user."}, status=status.HTTP_404_NOT_FOUND)
+        
+        orders_data = []
+        for order in orders:
+            order_data = OrderSerializer(order).data
+            order_data['total_quantity'] = order.total_quantity  
+            orders_data.append(order_data)
+        
+        return Response(orders_data)
+    
+class PopularProduct(APIView):
+    def get(self,request):
+        popular = OrderItem.objects.values('product').annotate(totalquantity=Sum('quantity')).order_by('totalquantity').first()
+
+        product = Product.objects.get(id=popular['product'])
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+class TotalQuantitySold(APIView):
+    def get(self,request):
+        sold = OrderItem.objects.values('product').annotate(t_quantity=Sum('quantity')).order_by('-t_quantity')
+
+        if not sold:
+            return Response({"detail": "No products found."}, status=status.HTTP_404_NOT_FOUND)
+        
+        data = []
+
+        for item in sold:
+            p_id = item['product']
+            t_quant = item['t_quantity']
+
+            product = Product.objects.get(id=p_id)
+
+            serializer = ProductSerializer(product)
+
+            product_final = serializer.data
+            product_final['quantity_total'] = t_quant
+
+            data.append(product_final)
+
+        return Response(data)
+
+class TotalRevenuePerProductView(APIView):
+    def get(self, request):
+        
+        products_revenue = Product.objects.annotate(
+            total_revenue=Sum(F('orderitem__quantity') * F('price')) 
+        ).order_by('-total_revenue')  
+
+       
+        if not products_revenue:
+            return Response({"detail": "No products found."}, status=status.HTTP_404_NOT_FOUND)
+
+      
+        product_data = []
+
+       
+        for product in products_revenue:
+            serializer = ProductSerializer(product)
+
+            product_info = serializer.data
+            product_info['total_revenue'] = product.total_revenue 
+
+           
+            product_data.append(product_info)
+
+        return Response(product_data)
